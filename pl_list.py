3c5d7be54287206459247de4a15bf0981a4b6678
Dan C Williams <dan.c.williams@gmail.com>
2016-07-25 10:29:15 -0500

    Corrected some wayward logic - ability to create empty lists
    Also added ability to catch empty lists

--- pl_list.py
+++ pl_list.py
@@ -1,5 +1,5 @@
 __author__ = "Dan C Williams"
-__version__ = "0.2"
+__version__ = "0.3"
 __date__ = "Jul-25-2016"
 __email__ = "dan.c.williams@gmail.com"
 __status__ = "Development"\
@@ -15,11 +15,12 @@ def main():
     blackList = ['!']
     splitList = []
     plList = []
+    plNamesDict = {}
     plDict = collections.defaultdict(list)
     plDictFinal = {}
     deleteList = []
     temporaryList = []
-    plDescription = []
+    plDescriptionList = []
     plDefaultRoute = []
     plDescDict = collections.defaultdict(list)
     plDefaultDict = collections.defaultdict(list)
@@ -39,10 +40,11 @@ def main():
         temporaryList = [] #Reset TEMP List
     
     for i, line in enumerate(splitList): #Grab PL name and description and place them
+        plNamesDict[line[2]] = ''
         if line[3] == 'description':    #in a list of lists for further processing
             temporaryList.append(line[2]) 
             temporaryList.append(' '.join(line[4::]))
-            plDescription.append(temporaryList)
+            plDescriptionList.append(temporaryList)
             deleteList.append(line)
         if line [6] == '0.0.0.0/0':
             temporaryList.append(line[2]) 
@@ -60,11 +62,11 @@ def main():
         plList.append(temporaryList)
         temporaryList = []
     
-    for key, value in plDescription:  #create plDescDict using the key and description string
+    for key, value in plDescriptionList:  #create plDescDict using the key and description string
         tempString = value
         plDescDict[key] = tempString
     
-    for key, value in plList:  #create plDefaultDict using the key and network
+    for key, value in plDefaultRoute:  #create plDefaultDict using the key and network
         plDefaultDict[key].append(netaddr.IPNetwork(value))
         
     for key, value in plList:  #create plDict using the key and network
@@ -77,17 +79,21 @@ def main():
     
     target = open('test_output.txt', 'w')
     
-    d = collections.OrderedDict(sorted(plDictFinal.items()))
+    d = collections.OrderedDict(sorted(plNamesDict.items()))
+
+    print(plDefaultDict)
     
-    for key, value in d.items():
+    for key, blank in d.items():
         seq_num = 5
+        print(str(key))
         if key in plDescDict:
             target.write("ip prefix-list " + str(key) + " description " + str(plDescDict[key]) + "\n")
-        for i, ip_address in enumerate(value):
-            if str(ip_address.netmask) == '255.255.255.255':
-                target.write("ip prefix-list " + str(key) + " seq " + str(seq_num) + " permit " + str(ip_address) + ("\n"))
-            else:
-                target.write("ip prefix-list " + str(key) + " seq " + str(seq_num) + " permit " + str(ip_address) + " le 32\n")
+        if key in plDictFinal:
+            for i, ip_address in enumerate(plDictFinal[key]):
+                if str(ip_address.netmask) == '255.255.255.255':
+                    target.write("ip prefix-list " + str(key) + " seq " + str(seq_num) + " permit " + str(ip_address) + ("\n"))
+                else:
+                    target.write("ip prefix-list " + str(key) + " seq " + str(seq_num) + " permit " + str(ip_address) + " le 32\n")
             seq_num += 5
         if key in plDefaultDict:
             target.write("ip prefix-list " + str(key) + " seq 500000 deny 0.0.0.0/0 le 32\n")
