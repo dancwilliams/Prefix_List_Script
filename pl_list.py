19b8f72b810a747aad410c0eb095bac9573eb7fa
Dan C Williams <dan.c.williams@gmail.com>
2016-07-27 11:35:40 -0500

    Version and Date Update

--- pl_list.py
+++ pl_list.py
@@ -1,6 +1,6 @@
 __author__ = "Dan C Williams"
-__version__ = "0.4"
-__date__ = "Jul-26-2016"
+__version__ = "0.41"
+__date__ = "Jul-27-2016"
 __email__ = "dan.c.williams@gmail.com"
 __status__ = "Development"
 
