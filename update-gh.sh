98706e9d942b5a133f38c68efb0f39e30a7c80b2
Dan C Williams <dan.c.williams@gmail.com>
2016-10-10 08:20:58 -0500

    commit #7

--- update-gh.sh
+++ update-gh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+set -e
 
 echo 'Checking for updates to push.'
 if grep -q True "run_pl_update"; then
--- update_pl.sh
+++ update_pl.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -e
+
 echo 'Testing run_pl_update'
 
 if grep -q True "run_pl_update"; then
   echo 'Running Update'
+  mkdir ORIGINAL_BACKUPS
+  mkdir CHG_SCRIPTS
+  mkdir ORIGINAL_BACKUPS_NEW
+  mkdir CHG_SCRIPTS_NEW
+  echo ls -ltr
   python pl_yaml_processing.py
 fi
 
