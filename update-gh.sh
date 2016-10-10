ceb9ea7a2f09564c2f369d658b15f1b77b8d9d1b
Dan C Williams <dan.c.williams@gmail.com>
2016-10-10 07:42:02 -0500

    Updated echos in update-gh.sh script to perform testing #3

--- update-gh.sh
+++ update-gh.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+
+
 echo 'Checking for updates to push.'
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh\n"
+if grep -q True "run_pl_update"; then
+  if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo -e "Starting to update gh\n"
 
-  #copy data we're interested in to other place
-  cp -R ORIGINAL_BACKUPS $HOME/ORIGINAL_BACKUPS_NEW
-  cp -R CHG_SCRIPTS $HOME/CHG_SCRIPTS_NEW
+    #copy data we're interested in to other place
+    cp -R ORIGINAL_BACKUPS $HOME/ORIGINAL_BACKUPS_NEW
+    cp -R CHG_SCRIPTS $HOME/CHG_SCRIPTS_NEW
 
-  #go to home and setup git
-  cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
+    #go to home and setup git
+    cd $HOME
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis"
 
-  #using token clone gh-pages branch
-  git clone --quiet --branch=plist_cicd https://${GH_TOKEN}@github.com/dancwilliams/Prefix_List_Script.git  plist_cicd > /dev/null
+    #using token clone gh-pages branch
+    git clone --quiet --branch=plist_cicd https://${GH_TOKEN}@github.com/dancwilliams/Prefix_List_Script.git  plist_cicd > /dev/null
 
-  #go into diractory and copy data we're interested in to that directory
-  cd ORIGINAL_BACKUPS
-  cp -Rf $HOME/ORIGINAL_BACKUPS_NEW/* .
+    #go into diractory and copy data we're interested in to that directory
+    cd ORIGINAL_BACKUPS
+    cp -Rf $HOME/ORIGINAL_BACKUPS_NEW/* .
 
-  cd CHG_SCRIPTS
-  cp -Rf $HOME/CHG_SCRIPTS_NEW/* .
+    cd CHG_SCRIPTS
+    cp -Rf $HOME/CHG_SCRIPTS_NEW/* .
   
-  #add, commit and push files
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to plist-cicd"
-  git push -fq origin plist_cicd > /dev/null
+    #add, commit and push files
+    git add -f .
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to plist-cicd"
+    git push -fq origin plist_cicd > /dev/null
 
-  echo -e "Done Upldating Github\n"
+    echo -e "Done Upldating Github\n"
+  fi
 fi
 echo 'Finished with check.'
