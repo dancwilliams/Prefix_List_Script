6a49aa24d6c73314203730167c9625237624acc0
Dan C Williams <dan.c.williams@gmail.com>
2016-10-10 08:48:00 -0500

    commit #13 - Fix file structure

--- update_pl.sh
+++ update_pl.sh
@@ -6,11 +6,14 @@ echo 'Testing run_pl_update'
 
 if grep -q True "run_pl_update"; then
   echo 'Running Update'
-  mkdir ORIGINAL_BACKUPS
-  mkdir CHG_SCRIPTS
   mkdir ORIGINAL_BACKUPS_NEW
   mkdir CHG_SCRIPTS_NEW
   python pl_yaml_processing.py
+  ret=$?
+  if [ $ret -ne 0 ]; then
+     #Handle failure
+     exit 1
+  fi
 else
   echo 'No Update'
 fi
