574b4895212071b0b3e7d9866802aaddbb072b6e
Dan C Williams <dan.c.williams@gmail.com>
2016-10-10 10:28:52 -0500

    Test running of scripts

--- update-gh.sh
+++ update-gh.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+set -o verbose
+
 echo 'Checking for updates to push.'
 if grep -q True "run_pl_update"; then
     echo -e "Starting to update gh\n"
 
     #copy data we're interested in to other place
-    cp -R ORIGINAL_BACKUPS $HOME/ORIGINAL_BACKUPS_NEW
+    #cp -R ORIGINAL_BACKUPS $HOME/ORIGINAL_BACKUPS_NEWcp -R ORIGINAL_BACKUPS $HOME/ORIGINAL_BACKUPS_NEW
     cp -R CHG_SCRIPTS $HOME/CHG_SCRIPTS_NEW
+    cp -R ORIGINAL_BACKUPS $HOME/ORIGINAL_BACKUPS_NEW
 
     #go to home and setup git
     cd $HOME
@@ -16,7 +19,7 @@ if grep -q True "run_pl_update"; then
     git config --global user.name "Travis"
 
     #using token clone gh-pages branch
-    git clone --branch=plist_cicd https://${GH_TOKEN}@github.com/dancwilliams/Prefix_List_Script.git #> /dev/null
+    git clone --branch=plist_cicd https://${GH_TOKEN}@github.com/dancwilliams/Prefix_List_Script.git > /dev/null
 
     #go into diractory and copy data we're interested in to that directory
     cd Prefix_List_Script/ORIGINAL_BACKUPS
@@ -27,7 +30,7 @@ if grep -q True "run_pl_update"; then
     cp -Rf $HOME/CHG_SCRIPTS_NEW/* .
 
     #add, commit and push files
-    git status >> /dev/null
+
     git add -f .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to plist-cicd [ci skip]"
     git push -fq origin plist_cicd > /dev/null
