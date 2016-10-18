e75f5d594626323e0798da1f751e346c9e380555
Dan C Williams <dan.c.williams@gmail.com>
2016-10-18 13:22:49 -0500

    Updated template logic

--- EXTRA_SCRIPTS/MANUAL_CREATE/generate_config.py
+++ EXTRA_SCRIPTS/MANUAL_CREATE/generate_config.py
@@ -4,27 +4,16 @@ import os.path
 
 ENV = Environment(loader=FileSystemLoader('./'))
 script_path = 'SCRIPTS/'
-allowed_script = os.path.join(script_path, 'allowed.txt')
-remediation_script = os.path.join(script_path, 'remediation.txt')
+
+script = os.path.join(script_path, 'script.txt')
+
 
 with open("config.yaml") as _:
     yaml_dict =  yaml.load(_)
 
-allowed_dict = yaml_dict['allowed']
+template = ENV.get_template("template.text")
 
-remediation_dict = yaml_dict['remediation']
-
-# Render template and print generated config to console
-template = ENV.get_template("allowed_template.text")
-#print(template.render(config=allowed_dict))
-
-with open(allowed_script, 'w') as outfile:
-    temp = template.render(config=allowed_dict)
+with open(script, 'w') as outfile:
+    temp = template.render(config=yaml_dict)
     outfile.write(temp)
 
-template = ENV.get_template("remediation_template.text")
-#print(template.render(config=remediation_dict))
-
-with open(remediation_script, 'w') as outfile:
-    temp = template.render(config=remediation_dict)
-    outfile.write(temp)
